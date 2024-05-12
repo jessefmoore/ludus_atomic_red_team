fd200eaf181a74972f8fa31af7d6a7c0679dede8
PrymalInstynct <zimzamfam@gmail.com>
2024-05-12 07:59:27 -0600

    making things simpler

--- files/install_atomics.bat
+++ files/install_atomics.bat
@@ -1,2 +1,4 @@
 IEX (IWR 'https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1' -UseBasicParsing);
+Install-AtomicRedTeam
+IEX (IWR 'https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1' -UseBasicParsing);
 Install-AtomicRedTeam -getAtomics
